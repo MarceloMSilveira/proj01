81012dbcc6ff7e1b85dff0d906526e8c0ff270ad
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-03-14 14:47:57 -0300

    pg setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg'
 
 const app = express();
 const port = 3000;
+const { Client } = pg
 
 let quiz = [
   { country: "France", capital: "Paris" },
